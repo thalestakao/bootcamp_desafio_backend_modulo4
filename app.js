fafdd058df0ebe10fbdf6bc468da32d50f3158b9
Thales Baliero Takáo <engc.thales@gmail.com>
2020-11-08 17:04:50 -0300

    feat: setup of cors heroku frontend

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3001',
+    origin: 'https://igti-frontend-grades-api.herokuapp.com/',
   })
 );
 
