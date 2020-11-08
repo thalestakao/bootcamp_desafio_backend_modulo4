7cf858a0022419a202dcfb3b82fed3ce5331e749
Thales Baliero Takáo <engc.thales@gmail.com>
2020-11-08 17:39:24 -0300

    fix: cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://igti-frontend-grades-api.herokuapp.com/',
+    origin: 'https://igti-frontend-grades-api.herokuapp.com',
   })
 );
 
